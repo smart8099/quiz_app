1366ec495871b79e0698e7b9f90e26831968ee4e
smart8099 <abdulbasitm810@gmail.com>
2024-01-22 18:25:03 +0000

    Simple quiz app in VueJS

---
+++ src/components/Results.vue
@@ -0,0 +1,33 @@
+<template>
+   <div class="result">
+        <div class="title">{{ results[resultIndex].title }}</div>
+        <div class="desc">
+            {{ results[resultIndex].desc }}
+        </div>
+    </div>
+  
+  
+</template>
+
+<script>
+export default {
+    name: "Results",
+    props: ['results','totalCorrect'],
+    computed: {
+        resultIndex(){
+            let index = 0;
+
+            this.results.forEach((element,i) => {
+                if (element.min <= this.totalCorrect && element.max >= this.totalCorrect){
+                    index = i
+                }
+                
+            });
+
+            return index;
+        }
+    }
+
+}
+</script>
+
